a5351e22019da06d150c67460d2fdf35ee3ef4bf
Varlen Pavani Neto <varlen.neto@radixeng.com.br>
2021-07-21 14:25:36 -0300

    Add project files.

---
+++ MiniSendMail/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace MiniSendMail
+{
+    partial class Sendmail
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fromTextBox = new System.Windows.Forms.TextBox();
+            this.From = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toTextBox = new System.Windows.Forms.TextBox();
+            this.subjectTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bodyTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hostTextBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fromTextBox
+            // 
+            this.fromTextBox.Location = new System.Drawing.Point(65, 16);
+            this.fromTextBox.Name = "fromTextBox";
+            this.fromTextBox.Size = new System.Drawing.Size(414, 23);
+            this.fromTextBox.TabIndex = 0;
+            this.fromTextBox.Text = "hi@email.com";
+            // 
+            // From
+            // 
+            this.From.AutoSize = true;
+            this.From.Location = new System.Drawing.Point(6, 19);
+            this.From.Name = "From";
+            this.From.Size = new System.Drawing.Size(35, 15);
+            this.From.TabIndex = 1;
+            this.From.Text = "From";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "To";
+            // 
+            // toTextBox
+            // 
+            this.toTextBox.Location = new System.Drawing.Point(65, 45);
+            this.toTextBox.Name = "toTextBox";
+            this.toTextBox.Size = new System.Drawing.Size(414, 23);
+            this.toTextBox.TabIndex = 3;
+            this.toTextBox.Text = "recipient@email.com";
+            // 
+            // subjectTextBox
+            // 
+            this.subjectTextBox.Location = new System.Drawing.Point(65, 74);
+            this.subjectTextBox.Name = "subjectTextBox";
+            this.subjectTextBox.Size = new System.Drawing.Size(414, 23);
+            this.subjectTextBox.TabIndex = 4;
+            this.subjectTextBox.Text = "Test E-mail Container";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Subject";
+            // 
+            // bodyTextBox
+            // 
+            this.bodyTextBox.Location = new System.Drawing.Point(6, 117);
+            this.bodyTextBox.Name = "bodyTextBox";
+            this.bodyTextBox.Size = new System.Drawing.Size(484, 357);
+            this.bodyTextBox.TabIndex = 6;
+            this.bodyTextBox.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bodyTextBox);
+            this.groupBox1.Controls.Add(this.fromTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.From);
+            this.groupBox1.Controls.Add(this.subjectTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.toTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(496, 480);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Content";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.portTextBox);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.hostTextBox);
+            this.groupBox2.Controls.Add(this.sendButton);
+            this.groupBox2.Location = new System.Drawing.Point(529, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(272, 480);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "SMTP Config";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Host";
+            // 
+            // hostTextBox
+            // 
+            this.hostTextBox.Location = new System.Drawing.Point(6, 48);
+            this.hostTextBox.Name = "hostTextBox";
+            this.hostTextBox.Size = new System.Drawing.Size(260, 23);
+            this.hostTextBox.TabIndex = 7;
+            this.hostTextBox.Text = "localhost";
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(83, 403);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(103, 39);
+            this.sendButton.TabIndex = 0;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Port";
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(6, 100);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(260, 23);
+            this.portTextBox.TabIndex = 10;
+            // 
+            // Sendmail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 503);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Sendmail";
+            this.Text = "MiniSendMail";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fromTextBox;
+        private System.Windows.Forms.Label From;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox toTextBox;
+        private System.Windows.Forms.TextBox subjectTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox bodyTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox hostTextBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MiniSendMail/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace MiniSendMail
+{
+    public partial class Sendmail : Form
+    {
+        public Sendmail()
+        {
+            InitializeComponent();
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            var host = hostTextBox.Text;
+            if (!int.TryParse(portTextBox.Text, out var port))
+            {
+                return;
+            }
+
+            var client = new SmtpClient(
+                host, port);
+
+            var message = new MailMessage(fromTextBox.Text, toTextBox.Text, subjectTextBox.Text, bodyTextBox.Text);
+
+            client.Send(message);
+        }
+    }
+}
